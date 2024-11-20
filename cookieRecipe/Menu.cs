76ec4734ffede1d9c8e0625ee88a7675d85a281c
Emmanuel Cordero <eec123manny@gmail.com>
2024-11-19 22:19:09 -0500

    class files seperated

---
+++ cookieRecipe/Menu.cs
@@ -0,0 +1,10 @@
+﻿public class Menu
+{
+    // Ingredient picked by user
+    protected int Ingredient { get; private set; }
+
+    public Menu(int ingredient)
+    {
+        Ingredient = ingredient;
+    }
+}
---
+++ cookieRecipe/SummarizedRecipe.cs
@@ -0,0 +1,44 @@
+﻿//public class SummarizedRecipe : IngredientDisplay
+//{
+//    public SummarizedRecipe(int ingredient) : base(ingredient)
+//    {
+//    }
+
+//    public void SummarizedRecipeMaker()
+//    {
+//        if (!menuIngredientId.Contains(0) || exit)
+//        {
+//            Console.WriteLine("Your Recipe is Complete");
+//        }
+
+//        string completeList = "";
+//        //Takes the list of typed numbers and prints them with correlating description.
+//        // Check if there are any items in menuIngredientRecorder
+//        if (!menuIngredientId.Any())
+//        {
+//            Console.WriteLine("Sorry, Your Cookie Book is Empty");
+//        }
+//        else
+//        {
+//            // Loop through each item and append it to completeList
+//            foreach (int menuIngredientRecord in menuIngredientId)
+//            {
+//                completeList += $"{menuIngredientRecord}, ";
+//            }
+
+//            // Remove the trailing comma and space
+//            completeList = completeList.TrimEnd(',', ' ');
+
+//            // Print the final list on a single line
+//            Console.WriteLine(completeList);
+//        }
+
+//        List<string> multipleCompleteList = new List<string>();
+
+//        multipleCompleteList.Add(completeList);
+
+//        Console.WriteLine(multipleCompleteList);
+
+//    }
+//}
+//}
